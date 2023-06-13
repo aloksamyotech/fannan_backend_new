5dedee831cd095da17afc2f5105369ab06f96f8b
harshit22sam <harshit@samyotech.com>
2023-06-13 12:04:33 +0530

    donr

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,16 @@
+import express from "express";
+import { usercontroller } from "../controllers/user/User.js";
 
+const UserRouter = express.Router();
 
+// User Registration
 
-import express from 'express'
-import { usercontroller } from '../controllers/user/User.js';
+const UserController = new usercontroller();
+UserRouter.post("/user/register", UserController.user_regisgtration);
+UserRouter.post("/user/login", UserController.user_login);
 
-const UserRouter = express.Router()
+// Get users by category id
 
+UserRouter.get("/user/by/category/:id");
 
-
-// User Registration 
-
-const UserController = new usercontroller
-UserRouter.post("/user/register", UserController.user_regisgtration)
-UserRouter.post("/user/login", UserController.user_login)
-
-
-// Get users by category id 
-
-UserRouter.get("/user/by/category/:id")
-
-
-
-
-export default UserRouter; 
+export default UserRouter;
