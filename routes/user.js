a0b767c5416fd5afa38b1af0502069f410fbaf7b
harshit22sam <harshit@samyotech.com>
2023-06-13 14:19:56 +0530

    new

--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,26 @@
-import express from "express";
-import { usercontroller } from "../controllers/user/User.js";
+// import express from "express";
+// import { usercontroller } from "../controllers/user/User.js";
 
 const UserRouter = express.Router();
 
 // User Registration
 
-import express from 'express'
-import { usercontroller } from '../controllers/user/User.js';
-import { Category_controller } from '../controllers/user/Category.js';
+import express from "express";
+import { usercontroller } from "../controllers/user/User.js";
+import { Category_controller } from "../controllers/user/Category.js";
 
 // Get users by category id
 
 // UserRouter.get("/user/by/category/:id");
 
+// User Registration
 
-// User Registration 
+const UserController = new usercontroller();
+UserRouter.post("/user/register", UserController.user_regisgtration);
+UserRouter.post("/user/login", UserController.user_login);
 
-const UserController = new usercontroller
-UserRouter.post("/user/register", UserController.user_regisgtration)
-UserRouter.post("/user/login", UserController.user_login)
+// Get users by category id
+const CategoryController = new Category_controller();
+UserRouter.get("/user/by/category/:id", CategoryController.CategoryById);
 
-
-// Get users by category id 
-const CategoryController = new Category_controller
-UserRouter.get("/user/by/category/:id", CategoryController.CategoryById)
-
-
-
-
-export default UserRouter; 
+export default UserRouter;
