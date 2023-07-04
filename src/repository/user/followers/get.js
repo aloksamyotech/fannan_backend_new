af48a2364da21f2a121b9837defe15b4b7b269f7
aloksamyotech <127102691+aloksamyotech@users.noreply.github.com>
2023-07-04 16:48:20 +0530

    a

---
+++ src/repository/user/followers/get.js
@@ -0,0 +1,51 @@
+import { CommonMessage } from "../../../helpers/CommonResp.js"
+import { StatusCodes } from "../../../helpers/StatusHelper.js"
+import { followersModel } from "../../../models/schemas/user/Follow.js"
+import { IsUserExist } from "../user/details.js"
+
+
+const allFollowersOfUser =  async (id) => {
+    const filter = { following_id: id }
+    const count = await followersModel.count(filter)
+    return count ? count : 0
+}
+
+const allFollowingByUser = async (id) => {
+    const filter = { followers_id: id }
+    const count = await followersModel.count(filter)
+    return count ? count : 0
+}
+
+export const getAllFollowers = async (id) => {
+
+    try {
+
+        const isUserExist = await IsUserExist(id)
+        if (!isUserExist) {
+            return {
+                status: StatusCodes.NOT_FOUND,
+                message: CommonMessage.UserNotExist
+            }
+        }
+
+        const total_followers = await allFollowersOfUser(id)
+        const total_following = await allFollowingByUser(id)
+        const user_data = {
+            TotalFollowers: total_followers,
+            TotalFollowing: total_following
+        }
+
+        return {
+            data: user_data,
+            status: StatusCodes.OK,
+        }
+    } catch (error) {
+        console.log(error)
+        return{
+            status : StatusCodes.INTERNAL_SERVER_ERROR,
+            message : CommonMessage.INTERNAL_SERVER_ERROR
+        }
+
+    }
+
+}
