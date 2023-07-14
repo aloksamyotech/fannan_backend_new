d8149337876268f635b7fea3ff6e596e115e525e
aloksamyotech <127102691+aloksamyotech@users.noreply.github.com>
2023-07-14 16:16:20 +0530

    resolved issue

--- src/config/main.js
+++ src/config/main.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const Dbconnection = async () => {
     try {
-        await mongoose.connect(`${process.env.DB_URL}${process.env.DB_NAME}`);
+        await mongoose.connect(`mongodb://127.0.0.1:27017/fannan`);
         console.log('mongodb connection established');
     } catch (error) {
         console.log("Error connecting to MongoDB")
--- src/repository/user/user/details.js
+++ src/repository/user/user/details.js
@@ -3,74 +3,77 @@ import moment from "moment"
 
 export const detailsById = async (req, res, next) => {
     try {
+
+        const isUserExist = await IsUserExist(req)
+        if (!isUserExist) {
+            return {
+                status: 404,
+                message: "User does not exist"
+            }
+        }
         const filter = {
             _id: req
         }
-        const userdata = await UserModel.findById(filter)
-        if (userdata) {
 
-            var project = {
-                "lastname": "$lastname",
-                "firstname": "$firstname",
-                "category": "$userdata.title",
-                "email": "$email",
-                "phone": "$phone",
-                "created_at": "$created_at",
-                "updated_at": "$updated_at",
-                "isvarified": "$isvarified",
-                "base_price": "$base_price",
-                "category": "$userdata.title"
 
-            }
-            const user_details = await UserModel.aggregate([
-                {
-                    $lookup: {
-                        from: "categories",
-                        localField: "category",
-                        foreignField: "_id",
-                        as: "userdata",
-                    },
+        var project = {
+            "lastname": "$lastname",
+            "firstname": "$firstname",
+            "category": "$userdata.title",
+            "email": "$email",
+            "phone": "$phone",
+            "created_at": "$created_at",
+            "updated_at": "$updated_at",
+            "isvarified": "$isvarified",
+            "base_price": "$base_price",
+            "category": "$userdata.title"
+
+        }
+        const user_details = await UserModel.aggregate([
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "category",
+                    foreignField: "_id",
+                    as: "userdata",
                 },
-                { $unwind: { path: "$userdata" } },
-                { $project: project },
+            },
+            { $unwind: { path: "$userdata" } },
+            { $project: project },
 
-            ]);
-            const data = user_details.map((item) => {
-                return {
-                    _id: item._id,
-                    firstname: item.firstname,
-                    lastname: item.lastname,
-                    email: item.email,
-                    phone: item.phone,
-                    category: item.category,
-                    isvarified: item.isvarified,
-                    total_views: item.total_views,
-                    base_price: item.base_price,
-                    created_at: moment(item.created_at).format('YYYY-MM-DD  , LT'),
-                    updated_at: moment(item.updated_at).format('YYYY-MM-DD , LT '),
-                }
-
-            })
-            const userDetails = []
-            for (let i = 0; i < data.length; i++) {
-                if (data[i]._id == req.params.id) {
-                    userDetails.push(data[i])
-                }
+        ]);
+        const data = user_details.map((item) => {
+            return {
+                _id: item._id,
+                firstname: item.firstname,
+                lastname: item.lastname,
+                email: item.email,
+                phone: item.phone,
+                category: item.category,
+                isvarified: item.isvarified,
+                total_views: item.total_views,
+                base_price: item.base_price,
+                created_at: moment(item.created_at).format('YYYY-MM-DD  , LT'),
+                updated_at: moment(item.updated_at).format('YYYY-MM-DD , LT '),
             }
 
-            return {
-                status: 200,
-                data: userDetails,
+        })
+        const userDetails = []
+        for (let i = 0; i < data.length; i++) {
+            if (data[i]._id == req) {
+                userDetails.push(data[i])
             }
         }
-        else {
-            return {
-                status: 404,
-                massege: "user not found"
-            }
+
+        return {
+            status: 200,
+            data: userDetails,
         }
+
+
 
     } catch (error) {
+        console.log(error)
         return {
             status: 500,
             massege: "something went wrong" + error.message
